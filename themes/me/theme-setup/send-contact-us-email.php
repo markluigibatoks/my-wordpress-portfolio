da411ddff4cb2337e478c102a1a88709e97b8bdf
Mark-Luigi <markluigibatoks@gmail.com>
2025-08-30 04:32:37 -0230

    debug: send email

--- themes/me/theme-setup/send-contact-us-email.php
+++ themes/me/theme-setup/send-contact-us-email.php
@@ -1,6 +1,10 @@
 <?php
 add_action('acf/save_post', 'send_contact_form_email', 20);
 function send_contact_form_email($post_id) {
+
+    error_log('send_contact_form_email fired for post ID: ' . $post_id);
+
+
     if ($post_id == 'Contact Me') {
         $name = get_field('name', $post_id);
         $email = get_field('email', $post_id);
@@ -15,6 +19,10 @@ function send_contact_form_email($post_id) {
         $body = ob_get_clean();
 
         $headers = ['Content-Type: text/html; charset=UTF-8'];
+
+        error_log('Name: ' . $name);
+error_log('Email: ' . $email);
+error_log('Message: ' . $message);
 
         wp_mail($to, $subject, $body, $headers);
     }
